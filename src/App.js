8f8a4307ea09755f03e5e1271ac3a7941c3bb0ba
Randolph Schreiner <Rschreiner83@gmail.com>
2021-05-24 16:09:28 -0400

    githubsearch

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React,{Component} from 'react';
-import Products from './Products';
-import JumboTronComponent from './Jumbotron';
-import UserForm from './UserForm';
+//import Products from './Products';
+//import JumboTronComponent from './Jumbotron';
+//import UserForm from './UserForm';
+import GitHub from './GitHub';
 
 class App extends Component{
   render(){
 
     return(
       <div>
-        <JumboTronComponent>This is the long 
-        sentence added to fill in for this props children.
-        </JumboTronComponent>
-       <UserForm/>
-       <Products/>
+       <GitHub/>
       </div>
     );
   }
 }
 
 export default App;
+
+
+/* <JumboTronComponent>This is the long 
+sentence added to fill in for this props children.
+</JumboTronComponent>
+<UserForm/>
+<Products/> */
